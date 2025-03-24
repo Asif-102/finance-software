84048e0068ed2dfa239615f3b7d0efa1286d7c71
Asif-102 <asif1zero2@gmail.com>
2025-03-24 17:23:01 +0600

    header logo created

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -3,6 +3,7 @@ import { SignedIn, UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div>
+      <p>Dahsboard</p>
       <p>This is an authenticated route</p>
       <SignedIn>
         <UserButton />
